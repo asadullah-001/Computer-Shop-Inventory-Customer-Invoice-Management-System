5c89dc8884745431329ccc8958072899eb8d2ac9
asadullah-001 <md.asadullah2003@gmail.com>
2026-08-25 02:13:10 +0600

    Delete Computer Shop Inventory_Customer_and_Invoice Management System directory

--- Computer Shop Inventory_Customer_and_Invoice Management System/src/com/computershop/utils/InvalidDiscountException.java
+++
@@ -1,7 +0,0 @@
-package com.computershop.utils;
-
-public class InvalidDiscountException extends Exception {
-    public InvalidDiscountException(String message) {
-        super(message);
-    }
-}
--- Computer Shop Inventory_Customer_and_Invoice Management System/src/com/computershop/utils/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.computershop.utils;
-
-public class ProductNotFoundException extends Exception {
-    public ProductNotFoundException(String message) {
-        super(message);
-    }
-}
